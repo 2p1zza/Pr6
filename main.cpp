e80ffe2c21913e119d601a408574b7c2bc2322df
2p1zza <alinarainbow31@gmail.com>
2020-11-21 04:57:52 +0700

    my_pow

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+double my_pow(unsigned int step, double chis)
+{
+	double k = 1;
+	for (int i = 1; i <= step; i++)
+	{
+		k = k * chis;
+	}
+	return k;
+}
+int main()
+{
+	unsigned int a;
+	double b, k;
+	cout << "enter the degree and number\n";
+	cin >> a >> b;
+	k = my_pow(a, b);
+	cout << " = " << k;
+}
